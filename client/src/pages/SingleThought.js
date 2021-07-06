af6cafaea50b922e90892f2ab212ebbbb06953bb
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-07-06 23:41:14 +0100

    client graphql queries and navigation

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import ReactionList from '../components/ReactionList';
@@ -26,7 +26,10 @@ const SingleThought = props => {
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            {thought.username}
+            <Link to={`/profile/${thought.username}`}>
+              {thought.username}
+            </Link>
+            
           </span>{' '}
           thought on {thought.createdAt}
         </p>
